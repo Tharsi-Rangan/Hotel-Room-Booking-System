8ffaf97336c72d7c8e44c2cf198984b9c0b2421d
Jathusan-Jesuthasan <jesuthasanjathusan@gmail.com>
2026-09-25 18:17:33 +0530

    fix(security): weak rate limit - login limiter to 5 attempts / 15 min

--- backend/src/middleware/access.limiter.js
+++ backend/src/middleware/access.limiter.js
@@ -19,7 +19,10 @@ const logger = require('./winston.logger');
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   max: 1000, // limit each IP to 1000 requests per `window` (here, per 10 minutes)
-  message: { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+  message: {
+    message:
+      'Too many login attempts from this IP, please try again after a 60 second pause'
+  },
   handler: async (req, res, _next, options) => {
     try {
       const LOGS_FOLDER = `${appRoot}/logs/limiter`;
@@ -42,7 +45,9 @@ const limiter = rateLimit({
         verbose: false
       });
 
-      const logMessage = `[${currentDateTime()}]\tTITLE: TOO MANY REQUEST\tMETHOD: ${req.method}\tURL: ${req.url}\tCLIENT: ${req.headers['user-agent']}\n`;
+      const logMessage = `[${currentDateTime()}]\tTITLE: TOO MANY REQUEST\tMETHOD: ${
+        req.method
+      }\tURL: ${req.url}\tCLIENT: ${req.headers['user-agent']}\n`;
 
       apiLimiterRotator.write(logMessage, 'utf8');
     } catch (err) {
@@ -50,20 +55,21 @@ const limiter = rateLimit({
     }
 
     // sending API error response
-    res.status(options.statusCode).send(errorResponse(
-      29,
-      'TOO MANY REQUEST',
-      options.message.message
-    ));
+    res
+      .status(options.statusCode)
+      .send(errorResponse(29, 'TOO MANY REQUEST', options.message.message));
   },
   standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false // disable the `X-RateLimit-*` headers
 });
 
 const apiLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 5, // limit each IP to 10 login requests per `window` (here, per 1 minutes)
-  message: { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+  windowMs: 15 * 60 * 1000,
+  max: 5, // limit each IP to 5 login requests per 15 minutes
+  message: {
+    message:
+      'Too many login attempts from this IP, please try again after 15 minutes'
+  },
   handler: async (req, res, _next, options) => {
     try {
       const LOGS_FOLDER = `${appRoot}/logs/limiter`;
@@ -86,7 +92,9 @@ const apiLimiter = rateLimit({
         verbose: false
       });
 
-      const logMessage = `[${currentDateTime()}]\tTITLE: TOO MANY REQUEST\tMETHOD: ${req.method}\tURL: ${req.url}\tCLIENT: ${req.headers['user-agent']}\n`;
+      const logMessage = `[${currentDateTime()}]\tTITLE: TOO MANY REQUEST\tMETHOD: ${
+        req.method
+      }\tURL: ${req.url}\tCLIENT: ${req.headers['user-agent']}\n`;
 
       apiLimiterRotator.write(logMessage, 'utf8');
     } catch (err) {
@@ -94,11 +102,9 @@ const apiLimiter = rateLimit({
     }
 
     // sending API error response
-    res.status(options.statusCode).send(errorResponse(
-      29,
-      'TOO MANY REQUEST',
-      options.message.message
-    ));
+    res
+      .status(options.statusCode)
+      .send(errorResponse(29, 'TOO MANY REQUEST', options.message.message));
   },
   standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false // disable the `X-RateLimit-*` headers
